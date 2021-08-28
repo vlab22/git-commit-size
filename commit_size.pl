e51c492b3456138116f0baba44828de18cc622b9
Victor Botelho <vlab22@gmail.com>
2021-08-28 23:31:33 +0200

    My modifications to the original code

--- commit_size.pl
+++ commit_size.pl
@@ -2,6 +2,8 @@
 foreach my $rev (`git rev-list --all --pretty=oneline`) {
   my $tot = 0;
   ($sha = $rev) =~ s/\s.*$//;
+  my ( $rev_desc ) = $rev=~ /( .*)\s*$/;
+  $rev_desc =~ s/^\s+|\s+$//g;
   foreach my $blob (`git diff-tree -r -c -M -C --no-commit-id $sha`) {
     $blob = (split /\s/, $blob)[3];
     next if $blob == "0000000000000000000000000000000000000000"; # Deleted
@@ -11,6 +13,8 @@ foreach my $rev (`git rev-list --all --pretty=oneline`) {
   }
   my $revn = substr($rev, 0, 40);
   if ($tot > 1000000) {
-    print "$tot $revn " . `git show --pretty="format:" --name-only $revn | wc -l`  ;
+	$mb_tot = $tot / 1024 / 1024;
+	$mb_tot_str = sprintf('%.2f', $mb_tot);
+    print "$mb_tot_str Mb $rev_desc $revn " . `git show --pretty="format:" --name-only $revn | wc -l`  ;
   }
 }
